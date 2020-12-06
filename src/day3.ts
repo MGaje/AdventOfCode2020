a291653a9699b813a8a5b799b6e432441ebf347e
Michael Gajewski <gajewski.mike@gmail.com>
2020-12-06 15:21:50 -0500

    Day3 - Slight refactoring by just having a single map instance for both part 1 and 2.

--- src/day3.ts
+++ src/day3.ts
@@ -6,21 +6,20 @@ export class Day3 extends BaseDay<number> {
 
     constructor() {
         super('../input/day3.txt');
+        this._map = new TobogganMap(this._input);
     }
 
     public runPart1(): number {
-        return new TobogganMap(this._input).getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 3), new TobogganMapMovement(DirectionEnum.South, 1));
+        return this._map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 3), new TobogganMapMovement(DirectionEnum.South, 1));
     }
 
     public runPart2(): number {
-        const map: TobogganMap = new TobogganMap(this._input);
-
         const slopes: number[] = [
-            map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 1), new TobogganMapMovement(DirectionEnum.South, 1)),
-            map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 3), new TobogganMapMovement(DirectionEnum.South, 1)),
-            map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 5), new TobogganMapMovement(DirectionEnum.South, 1)),
-            map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 7), new TobogganMapMovement(DirectionEnum.South, 1)),
-            map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 1), new TobogganMapMovement(DirectionEnum.South, 2))
+            this._map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 1), new TobogganMapMovement(DirectionEnum.South, 1)),
+            this._map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 3), new TobogganMapMovement(DirectionEnum.South, 1)),
+            this._map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 5), new TobogganMapMovement(DirectionEnum.South, 1)),
+            this._map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 7), new TobogganMapMovement(DirectionEnum.South, 1)),
+            this._map.getTreeCollisionCount(new TobogganMapMovement(DirectionEnum.East, 1), new TobogganMapMovement(DirectionEnum.South, 2))
         ];
 
         return slopes.reduce((acc, v) => acc * v, 1);
